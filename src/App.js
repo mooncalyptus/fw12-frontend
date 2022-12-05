72cc5f050b778bad55fe85a25a7060354d94491e
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2022-12-05 20:28:38 +0700

    created sign in form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 const App = ()=> {
   return(
     <BrowserRouter>
     <Routes>
-      <Route path="/signup" element={<SignUp/>} />
+      <Route path="/signin" element={<SignIn/>} />
     </Routes>
     </BrowserRouter>
   )
