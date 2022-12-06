e306a35c07d06b368397f42cbd7569304ca19821
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2022-12-06 17:15:21 +0700

    create sign in and homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import SignIn from "./pages/SignIn";
+import Homepage from "./pages/Homepage";
 
 const App = ()=> {
   return(
     <BrowserRouter>
     <Routes>
       <Route path="/signin" element={<SignIn/>} />
+      <Route path="/homepage" element={<Homepage/>} />
     </Routes>
     </BrowserRouter>
   )
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const SignIn = () => {
     return(
@@ -15,7 +16,9 @@ const SignIn = () => {
                 <div className="text-base mt-4">Password</div>
                 <input type="password" class="form-input w-full rounded placeholder:text-slate-400 mt-4" placeholder="Write your password"></input>
                 <div></div>
-                <button type="button" className="text-black bg-[#9ED5C5] hover:bg-blue-800 hover:text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mt-4 w-full">Sign In</button>
+                <>
+                <Link to="/homepage"><button type="button" className="text-black bg-[#9ED5C5] hover:bg-blue-800 hover:text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mt-4 w-full">Sign In</button> </Link>
+                </>
                 <div className="flex flex-col">
                 <div className="flex justify-center">Forgot your password? <span className="text-[#8EC3B0]"> Reset now </span></div>
                 <div className="flex justify-center">Don’t have an account? <span className="text-[#8EC3B0]"> Sign Up </span></div>
