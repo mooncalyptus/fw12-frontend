8b8769d758a60f39292c84807d687b2b72ebc9c5
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2022-12-08 10:19:24 +0700

    creating view all page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import SignIn from "./pages/SignIn";
 import Homepage from "./pages/Homepage";
+import ViewAll from "./pages/ViewAll";
 
 const App = ()=> {
   return(
@@ -9,6 +10,7 @@ const App = ()=> {
     <Routes>
       <Route path="/signin" element={<SignIn/>} />
       <Route path="/homepage" element={<Homepage/>} />
+      <Route path="/viewall" element={<ViewAll/>} />
     </Routes>
     </BrowserRouter>
   )
--- src/pages/ViewAll.js
+++ src/pages/ViewAll.js
@@ -1,107 +1,56 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-function ShowingGrid({data}){
-    const [isShown, setisShown] = useState(false)
+function DisplayData({data}){
     return(
-        <div className="grid justify-items-center">
-            <img src={"http://localhost:8888/uploads/".concat(data.picture)} className="w-40 rounded" alt={data.title}
-            onMouseEnter={()=> setisShown(true)}
-            onMouseLeave={()=> setisShown(false)}/>
-            {isShown && (
-                <div className="grid justify-items-center">
-                <div>{data.title}</div>
-                <div>{data.genre}</div>
-                <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800 mt-4">Details</button>
-                </div>
-            )}
+        <div className="grid justify-items-center my-20">
+        <img src={"http://localhost:8888/uploads/".concat(data.picture)} className="w-40 rounded" alt={data.title}/>
+        <div>{data.title}</div>
+        <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800 mt-4">Details</button>
         </div>
     )
 }
-function UpcomingGrid({data}){
-    const [isShown, setisShown] = useState(false)
-    return(
-        <div className="grid justify-items-center">
-            <img src={"http://localhost:8888/uploads/".concat(data.picture)} className="w-40 h-60 rounded" alt={data.title}
-            onMouseEnter={()=> setisShown(true)}
-            onMouseLeave={()=> setisShown(false)}/>
-                {isShown && (
-                <div className="grid justify-items-center">
-                <div className="grid justify-items-center">{data.title}</div>
-                <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800 mt-4">Details</button>
-                </div>
-                 )}
-        </div>
-    )
-}
-function Homepage(){
-    const [nowShowing, setNowShowing] = React.useState([])
-    const [upcomingMovies, setUpcomingMovies] = React.useState([])
+function ViewAll(){
+    const [viewAll, setViewAll] = React.useState([])
     React.useEffect(()=> {
-        getNowShowing().then((data)=> {
-            setNowShowing(data.results);
+        getViewAll().then((data)=> {
+            setViewAll(data.result);
         });
-        getUpcomingMovies().then((data)=> {
-            setUpcomingMovies(data.results);
-        });
-    },[]);
+    }, [])
 
-    const getNowShowing = async ()=> {
-        const {data} = await axios.get("http://localhost:8888/movies/nowShowing");
+    const getViewAll = async ()=> {
+        const {data} = await axios.get("http://localhost:8888/movies?limit=8");
         return data;
     }
-    const getUpcomingMovies = async()=> {
-        const {data} = await axios.get("http://localhost:8888/movies/upcoming");
-        return data;
-    }
-        return(
-            <>
-            <div className="font-mulish">
-                <div className="bg-white flex">
+
+    return (
+        <div className="font-mulish">
+                <div className="flex">
                 <div className="flex items-center grow mx-32">
                 <img src={require('../assets/images/logo.png')} className="w-32" alt="desc"/>
-                <div className="mx-20">Home</div>
-                <Link to="/viewall"><div className="mx-3">List Movie</div></Link>
+                <Link to ="/homepage"><div className="mx-20">Home</div></Link>
+                <div className="mx-3">List Movie</div>
                 </div>
-                <div className="mr-32">
+                <div className="mr-32 mt-4 flex">
                 <>
-                <Link to="/homepage"><button type="button" className="text-black bg-[#9ED5C5] hover:bg-blue-800 hover:text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mt-4 w-30">Sign In</button> </Link>
+                <img src={require('../assets/images/search.png')} className="h-6 mt-2" alt="desc"/>
+                <img src={require('../assets/images/profile.png')} className="h-10 ml-2.5" alt="desc"/>
                 </>
                 </div>
                 </div>
-    
-                <div className="grid grid-cols-2 my-12">
-                    <div className="grid content-center justify-center">
-                        <div className="text-[#A0A3BD] text-2xl">Nearest Cinema, Newest Movie, </div>
-                        <div className="text-[#9ED5C5] text-5xl font-bold">Find out now!</div>
-                    </div>
-                    <div className="grid justify-center content-center">
-                    <img src={require('../assets/images/header.png')} alt="desc"/>
-                    </div>
-                </div>
 
-                <div className="bg-[#F5F6F8] flex flex-col">
-                    <div className="mx-32 my-16">
-                        <div className="text-[#9ED5C5] text-2xl font-bold">Now Showing</div>
-                        <hr class="w-48 h-1 bg-[#8EC3B0] rounded"></hr>
-                    </div>
-                    <div className="ml-28 mr-8 grid grid-cols-5">
-                        {nowShowing?.map((items)=> {
-                            return(
-                                <React.Fragment key={String(items.id)}>
-                                <ShowingGrid data={items} />
-                                </React.Fragment>
-                            )
-                        })}
-                    </div>
+                <div className="bg-[#E5E5E5]">
+                <div className="mx-32 flex content-center">
+                   <div className="grow mt-6">
+                <div className="text-[#9ED5C5] text-2xl font-bold">List Movie</div>
+                   </div>
+                <div className="flex justify-end">
+                <input type="text" class="form-input rounded border-none placeholder:text-slate-400 w-1/4 mr-6 mt-4" placeholder="sort"></input>
+                <input type="text" class="form-input rounded border-none placeholder:text-slate-400 mt-4 " placeholder="search"></input>
                 </div>
-
-                <div className="mx-32 my-16 flex">
-                <div className="text-[#9ED5C5] text-2xl font-bold flex grow">Upcoming Movies</div>
-                <Link to="/viewall"><div className="text-[#9ED5C5] font-bold">View All</div></Link> 
                 </div>
-                <div className="ml-24 overflow-x-auto">
+                <div className="mx-8 mr-2 mt-10 flex flex-row overflow-x-auto">
                 <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">January</button>
                 <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">February</button>
                 <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">March</button>
@@ -115,36 +64,25 @@ function Homepage(){
                 <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">November</button>
                 <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">December</button>
                 </div>
-                <div className="ml-28 mt-14 grid grid-cols-5">
-                    {upcomingMovies?.map((items)=> {
+            <div className=" bg-white grid grid-cols-4 gap-4 mt-14">
+            {viewAll?.map((items)=> {
                             return(
-                                <React.Fragment key={String(items.id)}>
-                                <UpcomingGrid data={items} />
-                                </React.Fragment>
+                            <React.Fragment key={String(items.id)}>
+                            <DisplayData data={items} />
+                            </React.Fragment>
                             )
                         })}
-                    </div>
+            </div>
 
-                    <div className="flex justify-center content-center">
-                        <div className="drop-shadow-md w-3/5 mt-32">
-                            <div className="flex flex-col">
-                            <div className="flex justify-center">Be the vanguard of the</div>
-                            <div className="text-[#9ED5C5] text-5xl font-bold flex justify-center">Movietify</div>
-                            </div>
-                            <div className="flex justify-center mt-12">
-                            <input type="email" class="form-input rounded placeholder:text-slate-400 h-12 w-72" placeholder="Write your email"></input>
-                            <div className="ml-4 mt-0.5">
-                            <button type="button" class="focus:outline-none text-black bg-[#9ED5C5] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Join Now</button>
-                            </div>
-                            </div>
-                            <div className="flex flex-col mt-10">
-                                <div className="flex justify-center">By joining you as a Tickitz member,</div>
-                                <div className="flex justify-center">we will always send you the latest updates via email .</div>
-                            </div>
-                        </div>
-                    </div>
+            <div className="flex justify-center content-center space-x-4 my-8 py-8">
+                <button className="border border-[#9ED5C5] bg-[#9ED5C5] w-6 rounded">1</button>
+                <button className="border border-[#9ED5C5] w-6 rounded">2</button>
+                <button className="border border-[#9ED5C5] w-6 rounded">3</button>
+                <button className="border border-[#9ED5C5] w-6 rounded">4</button>
+            </div>
+            </div>
 
-                    <div className="flex flex-col mt-24 ml-32">
+            <div className="flex flex-col mt-24 ml-32">
                         <div className="grid grid-cols-4">
                             <div>
                             <img src={require('../assets/images/logo.png')} className="w-32" alt="desc"/>
@@ -190,12 +128,8 @@ function Homepage(){
                         </div>
                         </div>
                     </div>
-            </div>
-            </>
-        )
-    }
+        </div>
+    )
+}
 
-
-
-
-export default Homepage;
+export default ViewAll;
