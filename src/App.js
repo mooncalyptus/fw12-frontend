8bf033a74f3a6cbd4f8ff59b9ffe3c4b57405498
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2022-12-13 12:59:36 +0700

    added redux folder, create profile page, create navbar tag

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import OrderPage from "./pages/OrderPage";
 import PaymentPage from "./pages/PaymentPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = ()=> {
   return(
@@ -19,6 +20,7 @@ const App = ()=> {
       <Route path="/forgotpassword" element={<ForgotPassword/>} />
       <Route path="/orderpage" element={<OrderPage/>} />
       <Route path="/paymentpage" element={<PaymentPage/>} />
+      <Route path="/profilepage" element={<ProfilePage/>} />
     </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+
+function ProfilePage(){
+    return(
+        <div className="font-mulish">
+            <Navbar></Navbar>
+            
+        </div>
+    )
+}
+
+export default ProfilePage;
