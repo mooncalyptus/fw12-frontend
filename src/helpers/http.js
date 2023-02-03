552b7d71e3fc1b0eb0184fbd8880722e89efb26f
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2023-02-03 10:43:03 +0700

    vercel configuration

---
+++ src/helpers/http.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const http = (token) => {
+  const headers = {};
+  if (token) {
+    headers.authorization = "Bearer " + token;
+  }
+  const instance = axios.create({
+    // baseURL: 'http://localhost:8888',
+    baseURL: process.env.DATA_BACKEND || 'https://fw12-collab2-backend.vercel.app',
+    headers,
+    keepAlive: true,
+  });
+  return instance;
+};
+
+export default http;
+
